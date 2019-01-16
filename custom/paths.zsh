6f6a810fb5e690897db7f00c9dc4b0f45d36950d
Donald E. Willcox <eugene.willcox@gmail.com>
2019-01-15 17:03:34 -0800

    Paths for Jade.

--- custom/paths.zsh
+++ custom/paths.zsh
@@ -14,6 +14,13 @@ if [[ "$hostname" == "groot.astro.sunysb.edu" ]]; then
     ASTRO_HOME="$HOME"	
 fi
 
+# NVIDIA compilers
+if [[ "$hostname" == "jade" ]]; then
+    CUDA_HOME="/usr/local/cuda-10.0"
+    export PATH="$CUDA_HOME/bin:$PATH"
+    export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$LD_LIBRARY_PATH"
+fi
+
 # Add scripts to path
 export PATH="$HOME/scripts:$PATH"
 export PATH="$ASTRO_HOME/job-run-scripts:$PATH"
